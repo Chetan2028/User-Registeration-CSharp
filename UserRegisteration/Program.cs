4cb218de0ad692abe2f05034599021d342cf5e69
Chetan2028 <bmchetan2028@gmail.com>
2020-10-26 21:54:17 +0530

    [Chetan] Add . Added to test sample emails

--- UserRegisteration/Program.cs
+++ UserRegisteration/Program.cs
@@ -9,25 +9,25 @@ namespace UserRegisteration
             ///Creates a instance of Validator class
             Validator validator = new Validator();
 
-            //Console.WriteLine("Enter the first name");
-            //string firstName = Console.ReadLine();
-            //validator.ValidateFirstName(firstName);
+            Console.WriteLine("Enter the first name");
+            string firstName = Console.ReadLine();
+            validator.ValidateFirstName(firstName);
 
-            //Console.WriteLine("Enter the last name");
-            //string lastName = Console.ReadLine();
-            //validator.ValidateLastName(lastName);
+            Console.WriteLine("Enter the last name");
+            string lastName = Console.ReadLine();
+            validator.ValidateLastName(lastName);
 
             Console.WriteLine("Enter the email");
             string email = Console.ReadLine();
             validator.ValidateEmailAddress(email);
 
-            //Console.WriteLine("Enter your mobile number");
-            //string mobileNumber = Console.ReadLine();
-            //validator.ValidateMobileNumber(mobileNumber);
+            Console.WriteLine("Enter your mobile number");
+            string mobileNumber = Console.ReadLine();
+            validator.ValidateMobileNumber(mobileNumber);
 
-            //Console.WriteLine("Enter your Password");
-            //string password = Console.ReadLine();
-            //validator.ValidatePassword(password);
+            Console.WriteLine("Enter your Password");
+            string password = Console.ReadLine();
+            validator.ValidatePassword(password);
         }
     }
 }
